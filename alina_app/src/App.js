463cb6a631a897c19a32daaac3895e1dee98d7c3
sevindunimrod01lw <sevindu@longwapps.com>
2024-05-25 14:59:01 +0530

    homepage-responsive

--- alina_app/src/App.js
+++ alina_app/src/App.js
@@ -3,7 +3,7 @@ import Home from './Home/Home.js';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import Products from './Products/Products.js';
-import contents from './Products/Contents.js';
+// import contents from './Products/Contents.js';
 import AboutUs from './AboutUs/AboutUs.js';
 import Contact from './Contact/Contact.js';
 
